417e0c6191708707254dd512af587731ac19b141
Felipe Silva <felipesilva916@gmail.com>
2022-06-05 11:59:44 -0700

    fixed form issue

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -18,6 +19,10 @@ function App() {
       reminder: true
     }
   ]);
+
+  //add Task
+  const addTask = (task) => {};
+
   //delete task function
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -35,6 +40,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
